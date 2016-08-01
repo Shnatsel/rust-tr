716a017c8de7fd8e96d80c03a8202fa1fb15684f
Sergey Shnatsel Davidoff <shnatsel@gmail.com>
2016-08-01 16:29:17 +0300

    Read chars to replace from command line. We've got a working tr!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::env;
 use std::io::prelude::*;
 
 fn main() {
@@ -7,12 +8,19 @@ fn main() {
     let mut buffer = String::new();
     let mut result = String::new();
 
-    while stdin.read_line(&mut buffer).unwrap() > 0 {
-        for character in buffer.chars() {
-            if character == ' ' {result.push('_')} else {result.push(character)}
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 2 {
+        let char_to_replace = args[1].chars().nth(0).unwrap();
+        let char_to_insert = args[2].chars().nth(0).unwrap();
+        while stdin.read_line(&mut buffer).unwrap() > 0 {
+            for character in buffer.chars() {
+                if character == char_to_replace {result.push(char_to_insert)} else {result.push(character)};
+            }
+            print!("{}", result);
+            buffer.clear();
+            result.clear();
         }
-        print!("{}", result);
-        buffer.clear();
-        result.clear();
+    } else {
+        println!("Usage: tr [OPTION]... SET1 [SET2]");
     }
 }
