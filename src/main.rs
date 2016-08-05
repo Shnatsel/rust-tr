31a2bc8e27b335c7043af5faa813a5f1cd630f6a
Sergey Shnatsel Davidoff <shnatsel@gmail.com>
2016-08-05 16:25:44 +0300

    Finally implemented handling of command-line parameters specific to modes in a reliable, hopefully idiomatic manner

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::io::prelude::*;
 
 enum TrMode {
-    Replace(Vec<char>),
+    ReplaceWith(Vec<char>),
     Delete,
 }
 
@@ -12,13 +12,14 @@ fn main() {
     let mut stdin = stdin.lock();
     let mut buffer = String::new();
     let mut result = String::new();
-    let mut operation_mode = TrMode::Replace(Vec::new()); //default mode
+    let mut operation_mode = TrMode::ReplaceWith(Vec::new()); //default mode
     let mut squeeze_repeats = false;
     let mut chars_to_squeeze: Vec<char> = Vec::new();
     let mut complement_set = false;
     let mut truncate_set = false;
     let mut first_argument_requires_escaping_dash = true; //for GNU-compatible option parsing
 
+    // parsing of command-line arguments
     if env::args().count() > 2 {
         let mut first_non_option_argument = 0;
         for (arg_number, arg) in env::args().skip(1).enumerate() {
@@ -33,27 +34,30 @@ fn main() {
                      else {first_non_option_argument = arg_number; break},
             }
         }
-        match operation_mode {
-            TrMode::Replace(_) => operation_mode = TrMode::Replace(env::args().skip(1).nth(first_non_option_argument + 1).unwrap().chars().collect()),
-            _ => {},
-        }
-        //FIXME: temp hack
         let chars_to_replace: Vec<char> = env::args().skip(1).nth(first_non_option_argument).unwrap().chars().collect();
-        let char_to_replace = chars_to_replace[0]; //temp hack
-        let chars_to_insert = env::args().skip(1).nth(first_non_option_argument + 1).unwrap();
-        let char_to_insert = chars_to_insert.chars().nth(0).unwrap(); //temp hack
+        match operation_mode {
+            // if we're replacing, read the chars to replace with from command line
+            TrMode::ReplaceWith(_) => operation_mode = TrMode::ReplaceWith(env::args().skip(1).nth(first_non_option_argument + 1).unwrap().chars().collect()),
+            _ => {},
+        };
+
         // main loop
         while stdin.read_line(&mut buffer).unwrap() > 0 {
             for character in buffer.chars() {
-                //TODO: replace == with custom function to handle --complement and multiple chars
-                if chars_to_replace.contains(&character) {
+                if chars_to_replace.contains(&character) ^ complement_set {
                     match operation_mode {
-                        TrMode::Replace(_) => result.push(char_to_insert),
+                         //TODO: handle this as set instead of inserting single char
+                        TrMode::ReplaceWith(ref chars_to_insert) => result.push(chars_to_insert[0]),
                         TrMode::Delete => {}, //do not copy char to output buffer
                     }
-                } else {result.push(character)};
-            }
+                } else {
+                    result.push(character)
+                };
+            };
             print!("{}", result);
+            // output is line-buffered, but we do not always print \n,
+            // so without the following line we may never output anything
+            io::stdout().flush();
             buffer.clear();
             result.clear();
         }
